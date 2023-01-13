de96da60aacec3a3d992011dfd5ccd53aac68c6d
Psudhanshu02 <pathrabe.sudhanshu0202@gmail.com>
2023-01-14 01:51:41 +0530

    st

--- flask_app.py
+++ flask_app.py
@@ -15,7 +15,7 @@ class hello_world():
     def m(t):
         movie_name= t
         
-        movies = pd.read_csv('movies.csv')
+        movies = pd.read_csv('./movies.csv')
 
         selective_columns = ['genres', 'keywords','tagline', 'cast', 'director']
 
